c3df64622c049d8666396f6d63c2914df628706e
2ndHG <71225645+2ndHG@users.noreply.github.com>
2024-04-19 12:22:55 +0800

    Index V1

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoLink = 'https://youtu.be/wYDNaNWxH5c';
+
+function buttonAction(buttonName) {
+    switch (buttonName) {
+        case 'paper':
+            alert("Coming soon! Please wait for the publicaiton of the paper");
+            break;
+        case 'scholar':
+            alert("Coming soon! Please wait for the publicaiton of the paper");
+            break;
+        case 'video':
+            window.open(videoLink, '_blank');
+            break; 
+    }
+}
