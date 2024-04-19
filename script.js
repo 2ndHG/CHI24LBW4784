788f10b5b803a03c6b64d0e1c392e988f1912854
2ndHG <71225645+2ndHG@users.noreply.github.com>
2024-04-19 14:42:02 +0800

    V7

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const videoLink = 'https://youtu.be/wYDNaNWxH5c';
 function buttonAction(buttonName) {
     switch (buttonName) {
         case 'paper':
-            alert("Coming soon! Please wait for the publicaiton of the paper");
+            alert("Coming soon! Please wait for the publication of the paper");
             break;
         case 'scholar':
-            alert("Coming soon! Please wait for the publicaiton of the paper");
+            alert("Coming soon! Please wait for the publication of the paper");
             break;
         case 'video':
             window.open(videoLink, '_blank');
