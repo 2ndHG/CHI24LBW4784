c49b15d262c02d39b780f96063ac942a35835546
2ndHG <71225645+2ndHG@users.noreply.github.com>
2024-05-12 17:48:30 +0800

    paper site uploaded

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const videoLink = 'https://youtu.be/wYDNaNWxH5c';
 function buttonAction(buttonName) {
     switch (buttonName) {
         case 'paper':
-            alert("Coming soon! Please wait for the publication of the paper");
+            //alert("Coming soon! Please wait for the publication of the paper");
+            window.open("https://dl.acm.org/doi/10.1145/3613905.3651018")
             break;
         case 'scholar':
             alert("Coming soon! Please wait for the publication of the paper");
